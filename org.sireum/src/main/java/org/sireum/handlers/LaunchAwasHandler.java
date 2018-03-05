4b909d66f89fdfb4f5d743b8e15e235a7ef494be
Hariharan Thiagarajan <thari@santoslab.org>
2018-03-04 22:55:54 -0600

    dynamic loading of awas

--- org.sireum/src/main/java/org/sireum/handlers/LaunchAwasHandler.java
+++ org.sireum/src/main/java/org/sireum/handlers/LaunchAwasHandler.java
@@ -1,6 +1,7 @@
 package org.sireum.handlers;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -10,12 +11,14 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.osgi.framework.Bundle;
+import org.sireum.PreferenceValues.SerializerType;
 import org.sireum.aadl.skema.ast.Aadl;
 import org.sireum.awas.AADLBridge.AadlHandler;
 import org.sireum.awas.ast.Model;
@@ -30,21 +33,32 @@ public class LaunchAwasHandler extends AbstractSireumHandler {
 		}
 		this.setGenerator(generator);
 		Aadl model = (Aadl) super.execute(e);
-		Model awas = AadlHandler.buildAwasModel(model);
-		String str = PrettyPrinter.apply(awas);
-		String awasFile = writeGeneratedFile(e, "awas", str);
-		if(awasFile != null) {
-			Bundle sireumBundle = Platform.getBundle("org.sireum");
-			System.out.println(sireumBundle.getLocation());
-			try {
-				generateVisualizer(e, awasFile);
-			} catch (URISyntaxException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+		final IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		String s = serialize(model, SerializerType.JSON);
+		writeGeneratedFile(e, "json", s);
+		try {
+			Class<?> c = Class.forName("org.sireum.awas.AADLBridge.AadlHandler");
+			Method m = c.getDeclaredMethod("buildAwasModel", Aadl.class);
+			Model awas = (Model) m.invoke(null, model);
+			String str = PrettyPrinter.apply(awas);
+			String awasFile = writeGeneratedFile(e, "awas", str);
+			if (awasFile != null) {
+				Bundle sireumBundle = Platform.getBundle("org.sireum");
+				System.out.println(sireumBundle.getLocation());
+				try {
+					generateVisualizer(e, awasFile);
+				} catch (URISyntaxException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
+		} catch (Exception ex) {
+			String m = "Could not generate Awas code.  Please make sure Awas is present.\n\n"
+					+ ex.getLocalizedMessage();
+			MessageDialog.openError(window.getShell(), "Sireum", m);
 		}
 		return null;
 	}
