c396fede6e0dcff1359bce790d4be44bf4d864ac
Hariharan Thiagarajan <thari@santoslab.org>
2018-11-30 17:40:14 -0600

    working for simple case

--- org.sireum.aadl.osate.cli/src/org/sireum/aadl/osate/cli/Activator.java
+++ org.sireum.aadl.osate.cli/src/org/sireum/aadl/osate/cli/Activator.java
@@ -3,6 +3,7 @@ package org.sireum.aadl.osate.cli;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
@@ -15,7 +16,9 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext bundleContext) throws Exception {
+
 		Activator.context = bundleContext;
 	}
 
@@ -23,6 +26,7 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 	}
--- org.sireum.aadl.osate.cli/src/org/sireum/aadl/osate/cli/GenerateAir.java
+++ org.sireum.aadl.osate.cli/src/org/sireum/aadl/osate/cli/GenerateAir.java
@@ -5,15 +5,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -28,46 +26,57 @@ import org.osate.aadl2.Classifier;
 import org.osate.aadl2.SystemImplementation;
 import org.osate.aadl2.instance.SystemInstance;
 import org.osate.aadl2.instantiation.InstantiateModel;
-import org.osate.alisa.workbench.AlisaStandaloneSetup;
-import org.osate.categories.CategoriesStandaloneSetup;
-import org.osate.xtext.aadl2.Aadl2StandaloneSetupGenerated;
-import org.osate.xtext.aadl2.errormodel.ErrorModelStandaloneSetup;
+import org.osate.assure.tests.FullAlisaInjectorProvider;
+import org.osate.core.OsateCorePlugin;
+import org.osate.pluginsupport.PluginSupportUtil;
+import org.osate.testsupport.Aadl2InjectorProvider;
+import org.osate.xtext.aadl2.properties.ui.internal.PropertiesActivator;
 import org.sireum.aadl.osate.util.TestUtil;
 
 import com.google.inject.Injector;
-import com.rockwellcollins.atc.resolute.ResoluteStandaloneSetup;
 
+@SuppressWarnings("restriction")
 public class GenerateAir implements IApplication {
 
-	@SuppressWarnings("restriction")
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
+		context.applicationRunning();
 
-		Injector injector = new Aadl2StandaloneSetupGenerated().createInjectorAndDoEMFRegistration();
+		OsateCorePlugin.getDefault().start(Activator.getContext());
+		Aadl2InjectorProvider aip = new FullAlisaInjectorProvider();
+		Injector injector = aip.getInjector();
 
-		ErrorModelStandaloneSetup.doSetup();
-		CategoriesStandaloneSetup.doSetup();
-		AlisaStandaloneSetup.doSetup();
-		ResoluteStandaloneSetup.doSetup();
+		PropertiesActivator.getInstance();
 
+//		Injector injector = PropertiesActivator.getInstance()
+//				.getInjector(PropertiesActivator.ORG_OSATE_XTEXT_AADL2_PROPERTIES_PROPERTIES);
+//		OsateCorePlugin.getDefault()
+//				.registerInjectorFor(PropertiesActivator.ORG_OSATE_XTEXT_AADL2_PROPERTIES_PROPERTIES, injector);
+//		final ResourceDescriptionsProvider resourceDescProvider = injector
+//				.getInstance(ResourceDescriptionsProvider.class);
+//		injector.getAllBindings().keySet().forEach(it -> System.out.println(it.toString()));
 
-//		Aadl2InjectorProvider aip = new Aadl2InjectorProvider();
-//		aip.setupRegistry();
-//		Injector injector = aip.getInjector();
-
+		injector.injectMembers(this);
 		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
-		// System.out.println("Tetsing :" + resourceSet);
-		if (resourceSet != null) {
-			IWorkspace ws = ResourcesPlugin.getWorkspace();
-			resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
-			Aadl2Package.eINSTANCE.eClass();
-			// testHelper = new TestHelper<AadlPackage>();
-			// Injector injector = Guice.createInjector();
 
+		if (resourceSet != null) {
+			resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
 			final Map<?, ?> args = context.getArguments();
 			final String[] appArgs = (String[]) args.get("application.args");
 			for (final String arg : appArgs) {
 				System.out.println(arg);
+			}
+			String sys = null;
+			File r = new File(appArgs[0]);
+			ArrayList<String> l = new ArrayList<>();
+			for (File f : r.listFiles()) {
+				if (f.getName().endsWith(".aadl")) {
+					if (f.getName().equals(appArgs[1])) {
+						sys = readFile(f);
+					} else {
+						l.add(readFile(f));
+					}
+				}
 			}
 
 			Resource resource = loadFiles(appArgs[0], appArgs[1], resourceSet);
@@ -75,6 +84,7 @@ public class GenerateAir implements IApplication {
 				AadlPackage ap = (AadlPackage) resource.getContents().get(0);
 				SystemImplementation sysImpl = (SystemImplementation) getResourceByName(appArgs[2],
 						ap.getOwnedPublicSection().getOwnedClassifiers());
+
 				SystemInstance instance = InstantiateModel.instantiate(sysImpl);
 				String ir = TestUtil.getAir(instance);
 				Files.write(Paths.get(appArgs[3]), ir.getBytes());
@@ -104,26 +114,32 @@ public class GenerateAir implements IApplication {
 
 	public Resource loadFiles(String dirPath, String fileName, ResourceSet rs) {
 		try {
-			HashSet<String> files = new HashSet<String>(Files.list(Paths.get(dirPath)).map(Path::toFile)
+			HashSet<String> files = new HashSet<String>(Files.list(Paths.get(dirPath)).map(java.nio.file.Path::toFile)
 					.map(File::getName).filter(p -> p.endsWith(".aadl")).collect(Collectors.toList()));
 
 			if (files.contains(fileName)) {
 				files.remove(fileName);
 			}
-			files.stream().forEach(f -> System.out.println(dirPath + f));
-			System.out.flush();
+
+			try {
+				PluginSupportUtil.getContributedAadl().stream().forEach(f -> rs.createResource(f));
+			} catch (Exception e) {
+				System.out.println(e);
+				System.out.flush();
+			}
 			files.stream().forEach(f -> loadFile(dirPath + File.separator + f, rs));
 
 			return loadFile(dirPath + File.separator + fileName, rs);
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			System.out.println(e);
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	public Resource loadFile(String filePath, ResourceSet rs) {
+	private Resource loadFile(String filePath, ResourceSet rs) {
 		try {
 			// This way of constructing the URL works in JUnit plug-in and standalone tests
 			URL url = new URL("file:" + filePath);
@@ -134,6 +150,7 @@ public class GenerateAir implements IApplication {
 			}
 			return res;
 		} catch (IOException e) {
+			System.out.println("Unable to load file: " + filePath);
 			return null;
 		}
 	}
