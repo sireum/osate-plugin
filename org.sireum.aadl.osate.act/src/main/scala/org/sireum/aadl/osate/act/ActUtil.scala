1e4f88909fdd16314640f847633c3514e6895450
Jason Belt <belt@ksu.edu>
2019-01-03 12:54:20 -0600

    added call wrapper

--- org.sireum.aadl.osate.act/src/main/scala/org/sireum/aadl/osate/act/ActUtil.scala
+++ org.sireum.aadl.osate.act/src/main/scala/org/sireum/aadl/osate/act/ActUtil.scala
@@ -1,56 +1,21 @@
 package org.sireum.aadl.osate.act
 
 import java.io.File
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
 import org.sireum.aadl.ir.Aadl
-import org.sireum.{F, T, ISZ, None, Option, Some, String, Z}
-import org.osate.aadl2.Element
-import org.eclipse.xtext.nodemodel.util.NodeModelUtils
+import org.sireum.{ISZ, Option, Some, String}
 import org.eclipse.ui.console.MessageConsole
 import org.sireum.aadl.osate.act.handlers.ActPrompt
+import org.sireum.aadl.osate.util.ScalaUtil
 
-object ActUtil {
-  val ACT_CLASS_NAME = "org.sireum.aadl.act.Act"
-  
+object ActUtil {  
   def launchAct(prompt: ActPrompt, model: Aadl, ms: MessageConsole, workspaceRoot: File): Int = {
 
-    var ret = -1
-
-    val out = new PrintStream(ms.newMessageStream())
-    val outOld = System.out
-    val errOld = System.err
-    
-    System.setOut(out)
-    System.setErr(out)
-    
-    Console.withOut(out) {
-       Console.withErr(out) {
-         try {
-           val c = Class.forName(ACT_CLASS_NAME)
-           val m = c.getDeclaredMethod("run", classOf[File], classOf[Aadl], classOf[ISZ[String]], classOf[Option[File]])
-
-           val outDir: File = new File(prompt.getOptionOutputDirectory());
-           val auxDirs : ISZ[String] = if(prompt.getOptionCSourceDirectory != "") ISZ(prompt.getOptionCSourceDirectory()) else ISZ()
-           val aadlRootDir: Option[File] = Some(workspaceRoot)
-           
-          ret = m.invoke(null, outDir, model, auxDirs, aadlRootDir).asInstanceOf[Int].intValue()
-
-        } catch {
-          case e: Exception =>  
-            out.println("Exception raised when invoking ACT")
-            e.printStackTrace(out)
-        } finally {
-          out.flush
+    ScalaUtil.callWrapper("ACT", ms, () => {
+      val outDir: File = new File(prompt.getOptionOutputDirectory());
+      val auxDirs : ISZ[String] = if(prompt.getOptionCSourceDirectory != "") ISZ(prompt.getOptionCSourceDirectory()) else ISZ()
+      val aadlRootDir: Option[File] = Some(workspaceRoot)
       
-          try { if (out != null) out.close() }
-          catch { case e: Exception => e.printStackTrace() }
-        }
-      }
-    }
-    System.setOut(outOld)
-    System.setErr(errOld)
-    
-    ret
+      org.sireum.aadl.act.Act.run(outDir, model, auxDirs, aadlRootDir)
+    })
   }
 }
--- org.sireum.aadl.osate.arsit/src/main/scala/org/sireum/aadl/osate/arsit/ArsitUtil.scala
+++ org.sireum.aadl.osate.arsit/src/main/scala/org/sireum/aadl/osate/arsit/ArsitUtil.scala
@@ -1,73 +1,39 @@
 package org.sireum.aadl.osate.arsit
 
 import java.io.File
-import java.io.PrintStream
 import org.sireum.aadl.ir.Aadl
 import org.sireum.aadl.osate.arsit.handlers.ArsitPrompt
-import org.sireum.{F, T, ISZ, None, Option, Some, String, Z}
+import org.sireum.{F, ISZ, None, Option, Some, String}
 import org.eclipse.ui.console.MessageConsole
+import org.sireum.cli.Cli
+import org.sireum.aadl.osate.util.ScalaUtil
+
 
 object ArsitUtil {
 
-  val className = "org.sireum.aadl.arsit.Runner"
-  
-  // Separating this out as it refers to the Sireum v3 artifact Cli.ArsitOption which may not be 
-  // present in other builds (e.g. ACT)
   def launchArsit(prompt: ArsitPrompt, model: Aadl, ms: MessageConsole): Int = {
-
-    import org.sireum.cli.Cli
-    
-    val c = Class.forName(className)
-    val m = c.getDeclaredMethod("run", classOf[File], classOf[Aadl], classOf[Cli.ArsitOption])
-
-    val outDir: File = new File(prompt.getOptionOutputDirectory());
-
-    val ipcmech = prompt.getOptionIPCMechanism match {
-      case "Message Queue" => Cli.Ipcmech.MessageQueue
-      case "Shared Memory" => Cli.Ipcmech.SharedMemory
-      case _ => Cli.Ipcmech.MessageQueue
-    }
-
-    var ret = -1
-
-    val out = new PrintStream(ms.newMessageStream())
-    val outOld = System.out
-    val errOld = System.err
-    
-    System.setOut(out)
-    System.setErr(out)
-    
-    Console.withOut(out) {
-    Console.withErr(out) {
-      try {
-        val opts = Cli.ArsitOption(
-            help = "",
-            args = ISZ(),
-            json = F, // irrelevant since passing the aadl model directly
-            outputDir = Some(prompt.getOptionOutputDirectory),
-            packageName = if(prompt.getOptionBasePackageName == "") None[String] else Some(prompt.getOptionBasePackageName()),
-            noart = !prompt.getOptionEmbedArt,
-            bless = prompt.getOptionGenerateBlessEntryPoints,
-            genTrans = prompt.getOptionGenerateTranspilerArtifacts,
-            ipc = ipcmech
-          )
-          ret = m.invoke(null, outDir, model, opts).asInstanceOf[Int].intValue()
+    ScalaUtil.callWrapper("Arsit", ms, () => {
+      val outDir: File = new File(prompt.getOptionOutputDirectory())
   
-        } catch {
-          case e: Exception =>  
-            out.println("Exception raised when invoking ACT")
-            e.printStackTrace(out)
-        } finally {
-          out.flush
-      
-          try { if (out != null) out.close() }
-          catch { case e: Exception => e.printStackTrace() }
-        }
+      val ipcmech = prompt.getOptionIPCMechanism match {
+        case "Message Queue" => Cli.Ipcmech.MessageQueue
+        case "Shared Memory" => Cli.Ipcmech.SharedMemory
+        case _ => Cli.Ipcmech.MessageQueue
       }
-    }
-    System.setOut(outOld)
-    System.setErr(errOld)
-    
-    ret
+        
+      val opts = Cli.ArsitOption(
+        help = "",
+        args = ISZ(),
+        json = F, // irrelevant since passing the aadl model directly
+        outputDir = Some(prompt.getOptionOutputDirectory),
+        packageName = if(prompt.getOptionBasePackageName == "") None[String] else Some(prompt.getOptionBasePackageName()),
+        noart = !prompt.getOptionEmbedArt,
+        bless = prompt.getOptionGenerateBlessEntryPoints,
+        genTrans = prompt.getOptionGenerateTranspilerArtifacts,
+        ipc = ipcmech
+      )
+      
+      org.sireum.aadl.arsit.Runner.run(outDir, model, opts)
+    })
   }
 }
