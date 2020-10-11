0c6228e1fdd778d96ab66c31ec008889833ffff2
Jason Belt <belt@ksu.edu>
2020-10-11 21:12:29 +0000

    added air updater support for pca-pump

--- org.sireum.aadl.osate.tests/src/org/sireum/aadl/osate/tests/extras/AirUpdater.java
+++ org.sireum.aadl.osate.tests/src/org/sireum/aadl/osate/tests/extras/AirUpdater.java
@@ -1,35 +1,28 @@
 package org.sireum.aadl.osate.tests.extras;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.XtextRunner;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.osate.aadl2.AadlPackage;
-import org.osate.aadl2.Classifier;
 import org.osate.aadl2.SystemImplementation;
 import org.osate.aadl2.instance.SystemInstance;
 import org.osate.aadl2.instantiation.InstantiateModel;
 import org.osate.testsupport.Aadl2InjectorProvider;
 import org.osate.testsupport.TestResourceSetHelper;
+import org.sireum.aadl.osate.tests.extras.UpdaterUtil.TestAadlProject;
+import org.sireum.aadl.osate.tests.extras.UpdaterUtil.TestAadlSystem;
 import org.sireum.aadl.osate.util.Util;
 import org.sireum.aadl.osate.util.Util.SerializerType;
 
@@ -44,10 +37,12 @@ public class AirUpdater extends XtextTest {
 
 	@Test
 	public void updateAirHamr() {
-		File hamrModelsDir = new File(
-				System.getenv("SIREUM_HOME") + "/hamr/codegen/jvm/src/test/scala/models");
+		File hamrModelsDir = new File(System.getenv("SIREUM_HOME") + "/hamr/codegen/jvm/src/test/scala/models");
+
 		if (hamrModelsDir.exists()) {
-			regen(hamrModelsDir, ".slang");
+			for (TestAadlSystem system : UpdaterUtil.findSystems(hamrModelsDir)) {
+				regen(system);
+			}
 		} else {
 			System.out.println("Directory does not exist: " + hamrModelsDir);
 		}
@@ -55,164 +50,83 @@ public class AirUpdater extends XtextTest {
 
 	@Test
 	public void updateAirHamr2() {
-		File hamrModelsDir = new File(
-				"/home/vagrant/devel/case/CASE-loonwerks/TA5/tool-evaluation-4/HAMR/examples");
+
+		File hamrModelsDir = new File("/home/vagrant/devel/case/CASE-loonwerks/TA5/tool-evaluation-4/HAMR/examples");
 		if (hamrModelsDir.exists()) {
-			regen(hamrModelsDir, ".slang");
+			for(TestAadlSystem system : UpdaterUtil.findSystems(hamrModelsDir)) {
+				regen(system);
+			}
 		} else {
 			System.out.println("Directory does not exist: " + hamrModelsDir);
 		}
+
 	}
 
-	void regen(File rootDir, String outputDir) {
+	void regen(TestAadlSystem system) {
 
-		List<File> projectDirs = new ArrayList<>();
-		findProjects(rootDir, projectDirs);
+		SystemInstance instance = getSystemInstance(system);
+		assert instance != null : "System is null " + system.systemImplementationName;
 
-		for (File f : projectDirs) {
-			String projectName = getProjectName(f);
+		String air = Util.serialize(Util.getAir(instance, true), SerializerType.JSON);
 
-			List<File> aadlFiles = collectFiles(f, ".aadl");
-			List<File> systemDescriptions = collectFiles(f, ".system");
+		String instanceFilename = Util.toIFile(instance.eResource().getURI()).getName();
+		String fname = instanceFilename.substring(0, instanceFilename.lastIndexOf(".")) + ".json";
 
-			File sysFile = null;
-			String implName = null;
-			if (systemDescriptions.size() > 0) {
-				// .system files should have the name of the file that contains
-				// the system instance on the first line and the simple name
-				// of the system instance on the second line. e.g.
-				// UAV.aadl
-				// UAV.Impl
-				String[] testParams = readFile(systemDescriptions.get(0)).split("\n");
-				sysFile = new File(f, testParams[0]);
-				implName = testParams[1];
-			} else {
-				for (File a : aadlFiles) {
-					for (String line : readFile(a).split("\n")) {
-						String SYS_IMPL = "system implementation";
-						if (line.contains(SYS_IMPL)) {
-							assert sysFile == null : "Found multiple system implementations in " + f;
-							sysFile = a;
-							implName = line.substring(line.indexOf(SYS_IMPL) + SYS_IMPL.length()).trim();
-							break;
-						}
-					}
+		File outFile = new File(system.slangOutputDir, fname);
+		IOUtils.writeFile(outFile, air);
+		IOUtils.zipFile(outFile);
+	}
+
+	SystemInstance getSystemInstance(TestAadlSystem system) {
+		try {
+			ResourceSet rset = createResourceSet(system.projects);
+
+			Resource sysImplResource = null;
+			// TODO: determine correct way of getting the OSATE URI for the system impl file
+			for (Resource rs : rset.getResources()) {
+				String filename = rs.getURI().lastSegment();
+				if (filename.equals(system.systemImplementationFile.getName())) {
+					sysImplResource = rs;
 				}
 			}
-			assert sysFile != null && implName != null : String.format("Null things:\n%s\n%s\n%s", f, sysFile,
-					implName);
-			aadlFiles.remove(sysFile);
 
-			SystemInstance instance = getSystemInstance(projectName, sysFile, implName, aadlFiles);
-			String air = Util.serialize(Util.getAir(instance, true), SerializerType.JSON);
+			if (sysImplResource != null) {
+				final AadlPackage pkg = (AadlPackage) (sysImplResource.getContents().isEmpty() ? null : sysImplResource.getContents().get(0));
 
-			String instanceFilename = Util.toIFile(instance.eResource().getURI()).getName();
-			String fname = instanceFilename.substring(0, instanceFilename.lastIndexOf(".")) + ".json";
+				SystemImplementation sysImpl = (SystemImplementation) UpdaterUtil.getResourceByName(system.systemImplementationName,
+						pkg.getOwnedPublicSection().getOwnedClassifiers());
 
-			writeFile(new File(f, outputDir + "/" + fname), air);
+				return InstantiateModel.instantiate(sysImpl);
+			} else {
+				throw new RuntimeException("Couldn't find resource " + system.systemImplementationFile);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
+		System.exit(1);
+		return null;
 	}
 
-	SystemInstance getSystemInstance(String projectName, File sysImplFile, String sysImplName, List<File> aadlFiles) {
-		try {
+	ResourceSet createResourceSet(List<TestAadlProject> projects) {
 
+		for (TestAadlProject project : projects) {
 			// pretend the files are organized in a project called <projectName> so that URIs in
 			// AIR Positions are relative to the project, not the file system
 			// https://github.com/osate/osate2/wiki/Using-contributed-resources-in-stand-alone-applications/d5e28542e20b531b8688ab962aa08606d5e619a8
-			String wsRoot = sysImplFile.getParentFile().getAbsolutePath();
-			EcorePlugin.getPlatformResourceMap().put(projectName, URI.createFileURI(wsRoot));
-
-			AadlPackage pkg = parseFile(projectName, sysImplFile, aadlFiles.toArray(new File[aadlFiles.size()]));
-
-			// instantiate
-			SystemImplementation sysImpl = (SystemImplementation) getResourceByName(sysImplName,
-					pkg.getOwnedPublicSection().getOwnedClassifiers());
-			return InstantiateModel.instantiate(sysImpl);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			Assert.assertFalse(true);
-			return null;
+			String wsRoot = project.rootDirectory.getAbsolutePath();
+			EcorePlugin.getPlatformResourceMap().put(project.projectName, URI.createFileURI(wsRoot));
 		}
-	}
 
-	void findProjects(File f, List<File> l) {
-		if (f.isDirectory()) {
-			for (File file : f.listFiles()) {
-				findProjects(file, l);
-			}
-		} else {
-			if (f.getName().equals(".project")) {
-				l.add(f.getParentFile());
-			}
-		}
-	}
-
-	private String getProjectName(File f) {
-		String marker = "<name>";
-		String line = readFile(new File(f, ".project")).split("\n")[2];
-		return line.substring(line.indexOf(marker) + marker.length(), line.indexOf("</name>"));
-	}
-
-	List<File> collectFiles(File dir, String endsWith) {
-		return Arrays.asList(dir.listFiles()).stream()
-				.filter(p -> p.isFile() && p.getName().endsWith(endsWith)).collect(Collectors.toList());
-	}
-
-	String readFile(File f) {
-		try {
-			return new String(Files.readAllBytes(Paths.get(f.toURI())));
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-			return null;
-		}
-	}
-
-	void writeFile(File f, String str) {
-		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(f.toURI()))) {
-			writer.write(str);
-			System.out.println("Wrote: " + f);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
-
-	Classifier getResourceByName(String name, EList<Classifier> l) {
-		for (Classifier oc : l) {
-			if (oc.getName().equals(name)) {
-				return oc;
-			}
-		}
-		return null;
-	}
-
-	/** Adapted from
-	 * <a href="https://github.com/osate/osate2/blob/44af9ff8d6309410aeb134a0ae825aa7c916fabf/core/org.osate.testsupport/src/org/osate/testsupport/TestHelper.java#L126"/>here</a>.
-	 *
-	 * Parse a set of files into the test resource set.
-	 * They can be of any extension supported by the InjectorProvider injected into the JUnit test class
-	 * The first file is assumed to have a root object of type T
-	 *
-	 * @param projectName root of paths relative to the resource set
-	 * @param filePath the main file to parse
-	 * @param referenced other files that may be referenced by the main file
-	 * @return the root object of the main file
-	 */
-	public AadlPackage parseFile(String projectName, File file, File... referencedPaths) {
 		ResourceSet rs = rsHelper.getResourceSet();
-		for (File name : referencedPaths) {
-			loadFile(projectName, name, rs);
+		for (TestAadlProject project : projects) {
+			for (File f : project.aadlFiles) {
+				loadFile(project.projectName, f, rs);
+			}
 		}
-		Resource res = loadFile(projectName, file, rs);
-		if (res != null) {
-			@SuppressWarnings("unchecked")
-			final AadlPackage root = (AadlPackage) (res.getContents().isEmpty() ? null : res.getContents().get(0));
-			return root;
-		}
-		return null;
+		return rs;
 	}
+
 
 	/** Adapted from
 	 * <a href="https://github.com/osate/osate2/blob/44af9ff8d6309410aeb134a0ae825aa7c916fabf/core/org.osate.testsupport/src/org/osate/testsupport/TestHelper.java#L147">here</a>.
---
+++ org.sireum.aadl.osate.tests/src/org/sireum/aadl/osate/tests/extras/UpdaterUtil.java
@@ -0,0 +1,160 @@
+package org.sireum.aadl.osate.tests.extras;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+import org.osate.aadl2.Classifier;
+
+public class UpdaterUtil {
+
+	static Classifier getResourceByName(String name, EList<Classifier> l) {
+		for (Classifier oc : l) {
+			if (oc.getName().equals(name)) {
+				return oc;
+			}
+		}
+		return null;
+	}
+
+	static String getProjectName(File projectFile) {
+		assert (projectFile.getName().equals(".project"));
+
+		String marker = "<name>";
+		String line = IOUtils.readFile(projectFile).split("\n")[2];
+		return line.substring(line.indexOf(marker) + marker.length(), line.indexOf("</name>"));
+	}
+
+	static List<TestAadlSystem> findSystems(File f) {
+		List<TestAadlSystem> ret = new ArrayList<TestAadlSystem>();
+		if (f.isDirectory()) {
+			List<File> systems = IOUtils.collectFiles(f, ".system", false);
+			if (!systems.isEmpty()) {
+				// found user provided .system file so use that
+
+				assert (systems.size() == 1);
+				ret.add(UpdaterUtil.createTestAadlSystem(systems.get(0)));
+				return ret;
+			}
+			for (File file : f.listFiles()) {
+				ret.addAll(findSystems(file));
+			}
+		} else {
+			if (f.getName().equals(".project")) {
+				// assumes there is only one system implementation in the
+				// directory rooted at f
+
+				String projectName = UpdaterUtil.getProjectName(f);
+				File rootDirectory = f.getParentFile();
+				List<File> aadlFiles = IOUtils.collectFiles(rootDirectory, ".aadl", true);
+				TestAadlProject project = new TestAadlProject(projectName, rootDirectory, aadlFiles);
+
+				String systemImpl = null;
+				File systemFile = null;
+				for (File a : aadlFiles) {
+					for (String line : IOUtils.readFile(a).split("\n")) {
+						String SYS_IMPL = "system implementation";
+						if (line.contains(SYS_IMPL)) {
+							assert systemFile == null : "Found multiple system implementations in " + f.getParent();
+							systemFile = a;
+							systemImpl = line.substring(line.indexOf(SYS_IMPL) + SYS_IMPL.length()).trim();
+							break;
+						}
+					}
+				}
+				ret.add(new TestAadlSystem(systemImpl, systemFile, List.of(project)));
+			}
+		}
+		return ret;
+	}
+
+	static TestAadlSystem createTestAadlSystem(File systemFile) {
+		try {
+
+			String[] _projects = TestAadlSystem.getProjectsProperty(systemFile);
+			File systemImplFile = TestAadlSystem.getSystemImplementationFile(systemFile);
+			String systemImpl = TestAadlSystem.getSystemImplementationClassifierName(systemFile);
+
+			List<TestAadlProject> projects = new ArrayList<TestAadlProject>();
+			for (String p : _projects) {
+				File projectFile = new File(systemFile.getParentFile(), p);
+				assert (projectFile.exists() && projectFile.isFile() && projectFile.getName().equals(".project"));
+
+				String projectName = getProjectName(projectFile);
+				List<File> aadlFiles = IOUtils.collectFiles(projectFile.getParentFile(), ".aadl", true);
+
+				projects.add(new TestAadlProject(projectName, projectFile.getParentFile(), aadlFiles));
+			}
+			return new TestAadlSystem(systemImpl, systemImplFile, projects);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	static class TestAadlProject {
+		String projectName;
+		File rootDirectory;
+		List<File> aadlFiles;
+
+		TestAadlProject(String _projectName, File _rootDirectory, List<File> _aadlFiles) {
+			assert (_projectName != null);
+			assert _rootDirectory != null && _rootDirectory.exists() && _rootDirectory.isDirectory();
+			assert !_aadlFiles.isEmpty() : "no aadl files for " + projectName + " in root dir " + rootDirectory;
+
+			this.projectName = _projectName;
+			this.rootDirectory = _rootDirectory;
+			this.aadlFiles = _aadlFiles;
+		}
+	}
+
+	static class TestAadlSystem {
+
+		String systemImplementationName;
+		File systemImplementationFile;
+		File slangOutputDir;
+		List<TestAadlProject> projects;
+
+		TestAadlSystem(String _systemImpl, File _systemFile, List<TestAadlProject> _projects) {
+			assert (_systemImpl != null);
+			assert (_systemFile != null && _systemFile.exists() && _systemFile.isFile());
+			assert (_projects.size() > 0);
+
+			// assumes first project has the .slang directory
+			slangOutputDir = new File(_projects.get(0).rootDirectory, ".slang");
+			assert (slangOutputDir.exists() && slangOutputDir.isDirectory());
+
+			this.systemImplementationName = _systemImpl;
+			this.systemImplementationFile = _systemFile;
+			this.projects = _projects;
+		}
+
+		/*
+		 * Example .system properties file
+		 *
+		 * projects=pca/.project;BLESS_Resources/.project;ice-device/.project;physical/.project
+		 * system_impl_file=pca/aadl/packages/PCA_System.aadl
+		 * system_impl=wrap_pca.imp
+		 *
+		 */
+		static String KEY_PROJECT = "projects";
+		static String KEY_SYSTEM_IMPL_FILE = "system_impl_file";
+		static String KEY_SYSTEM_IMPL = "system_impl";
+
+		public static String getSystemImplementationClassifierName(File systemFile) {
+			return IOUtils.getPropertiesFile(systemFile).getProperty(KEY_SYSTEM_IMPL);
+		}
+
+		static String[] getProjectsProperty(File systemFile) {
+			return IOUtils.getPropertiesFile(systemFile).getProperty(KEY_PROJECT).split(";");
+		}
+
+		public static File getSystemImplementationFile(File systemFile) {
+			String systemImplFilename = IOUtils.getPropertiesFile(systemFile).getProperty(KEY_SYSTEM_IMPL_FILE);
+			return new File(systemFile.getParentFile(), systemImplFilename);
+		}
+
+	}
+}
