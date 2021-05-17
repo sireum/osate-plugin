f91d9ff75acfc5c230a29eca4b1de26693b6d7f0
Jason Belt <belt@ksu.edu>
2021-05-17 22:31:27 +0000

    support ba if/ifels/else statements

--- org.sireum.aadl.osate/src/main/java/org/sireum/aadl/osate/architecture/VisitorUtil.java
+++ org.sireum.aadl.osate/src/main/java/org/sireum/aadl/osate/architecture/VisitorUtil.java
@@ -169,4 +169,14 @@ public class VisitorUtil {
 		}
 		return ret;
 	}
+
+	public static class Pair<T1, T2> {
+		public final T1 t1;
+		public final T2 t2;
+
+		public Pair(T1 t1, T2 t2) {
+			this.t1 = t1;
+			this.t2 = t2;
+		}
+	}
 }
